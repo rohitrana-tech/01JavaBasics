e6e235075da97359fc6faee59cf7341aeb4156c6
Rohit Rana <rohitrana.tech@gmail.com>
2026-01-20 14:18:03 +0530

    If Else Statement

--- src/conditionalStatement/IfElse.java
+++ src/conditionalStatement/IfElse.java
@@ -23,29 +23,103 @@ public class IfElse {
 //        }
 
 
-        // You are given three numbers , find the maximum number
+// You are given three numbers , find the maximum number
+
+//        int a = 10;
+//        int b = 5;
+//        int c = 15;
+//
+//        if (a > b) {
+//            if (a > c) {
+//                System.out.println("Maximum number is: " + a);
+//            } else {
+//                System.out.println("Maximum number is: " + c);
+//            }
+//        } else {
+//            if (b > c) {
+//                System.out.println("Maximum number is: " + b);
+//            } else {
+//                System.out.println("Maximum number is: " + c);
+//            }
+//        }
 
 
-        int a = 12;
-        int b = 6;
-        int c = 9;
+// You are given four numbers , find the maximum number (using nested if ).
 
-        if(a>b) {
-            System.out.println(" a is Greater than b");
-            if (a>c) {
-                System.out.println(" a is Greater than c");
-            }
-                else {
-                    System.out.println(" c is Maximum");
-                }
-        } else  {
-            System.out.println("b is bigger than a");
-            if(b > c){
-                System.out.println("b is bigger than c");
-            }
-            
+
+//       int a = 100;
+//       int b = 20;
+//       int c = 15;
+//       int d = 25;
+//
+//       if(a > b){
+//           if(a > c){
+//               if(a > d){
+//                   System.out.println("Maximum number is: " + a);
+//               }else {
+//                   System.out.println("Maximum number is: " + d);
+//               }
+//           }
+//           else{
+//               if(c > d){
+//                   System.out.println("Maximum number is: " + c);
+//               }else{
+//                   System.out.println("Maximum number is: " +d);
+//               }
+//           }
+//       }
+//
+//       else{
+//           if(b>c){
+//               if(b>d){
+//                   System.out.println("Maximum number is:" + b);
+//               }
+//               else {
+//                   System.out.println("Maximum number is:" +d);
+//               }
+//           }
+//           else{
+//               if(c>d){
+//                   System.out.println("Maximum number is:" +c);
+//               }
+//               else {
+//                   System.out.println("Maximum number is:" +d);
+//               }
+//           }
+//       }
+
+
+
+       // Ternary Operator
+      // condition ? expression1 : expression2;
+
+//        int a = 12;
+//        int b = 24;
+//        int c = 20;
+//        int max = 0;
+//
+//        max = a > b ? a > c ? a : c : b > c ? b : c ;
+//
+//        System.out.println("value of max is : " +max);
+
+
+
+// UseCase of logical operators
+
+        int time = 12;
+
+        if(time >= 10 && time <= 20){
+            System.out.println("Office is open ");
+        }else{
+            System.out.println("Office is closed");
         }
 
+
+        if(time == 12 || time == 18){
+            System.out.println("Time to eat");
+        }else{
+            System.out.println("Time for work");
+        }
 
     }
 }
